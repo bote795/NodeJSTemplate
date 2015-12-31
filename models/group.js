080c0d3164c8ae8cd716f8825fefadaba60077db
bote795 <bote795@tamu.edu>
2015-12-31 15:07:01 -0600

    updated group schema

--- models/group.js
+++ models/group.js
@@ -3,8 +3,8 @@ var Schema = mongoose.Schema;
 var Group = new Schema({
     name: String,
     desc: String,
-    player_inv: [],
-    game_inv: [],
+    player_inv: [{ username: String, _Id: mongoose.Schema.Types.objectId, ref: 'Account'}],
+    game_inv: [{ name: String, _Id: mongoose.Types.Schema.objectId, ref: 'Game'}],
 });
 
 module.exports = mongoose.model('Group', Group);
