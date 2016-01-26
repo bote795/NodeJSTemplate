157b3735f5e7a5272fb9e00dc7b49a810edf98a3
bote795 <nbotell2@gmail.com>
2016-01-26 12:00:08 -0600

    working on selction of previous avatars

---
+++ public/js/chooseAvatar.js
@@ -0,0 +1,14 @@
+var parent = document.getElementById('pAvatars');
+parent.addEventListener('click',select,false);
+function select (e) {
+	function removeSelected (element) {
+		element.classList.remove("selected");
+	}
+	var items = document.getElementsByClassName('avatars');
+	for (var i = 0; i < items.length; i++) {
+		removeSelected(items[i]);
+	};
+	
+	if (e.target != parent)
+		e.target.classList.add("selected");
+}
