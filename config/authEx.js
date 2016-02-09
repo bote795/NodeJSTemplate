4c4136af6bb1b0251312c9532571964743361cfd
bote795 <nbotell2@gmail.com>
2016-02-09 11:23:47 -0600

    example of auth.js for later reference

---
+++ config/authEx.js
@@ -0,0 +1,39 @@
+//change name to auth,js
+module.exports = {
+    
+     db: process.env.DB ||'mongodb://'+ip+'/<name>',
+     app: {
+        name: 'NAME OF APP'
+      },
+      //https://developers.facebook.com/
+      //choose an app go to settings then go to advanced
+    'facebookAuth' : {
+        'clientID'      : 'DEFAULT_APP_ID', // your App ID
+        'clientSecret'  : 'APP_SECRET', // your App Secret
+        'callbackURL'   : 'https://localhost:3000/auth/facebook/callback',
+        'enabled'       : true
+    },
+
+    /*
+        setting up
+        http://stackoverflow.com/questions/24352975/passport-google-oauth-on-localhost
+        enable google+ API
+    */
+    'googleAuth' : {
+        'clientID'      : 'DEFAULT_APP_ID',
+        'clientSecret'  : 'APP_SECRET',
+        'callbackURL'   : 'https://localhost:3000/auth/google/callback',
+        'enabled'       : true
+    },
+    emailFrom: 'SENDER EMAIL ADDRESS', //  sender address like ABC <abc@example.com>
+    mailer: {
+        service: 'SERVICE_PROVIDER', // Gmail, SMTP
+        auth: {
+          user:  'EMAIL_ID',
+          pass: 'PASSWORD',
+          key: 'YOUR_AMAZON_KEY',
+          secret_key: 'YOUR_AMAZON_SECRET_KEY'
+        },
+    }, 
+    secret: 'SOME_TOKEN_SECRET'
+};
